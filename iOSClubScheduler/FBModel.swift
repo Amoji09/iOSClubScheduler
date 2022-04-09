0db6ef86d234581eb455c7fac2726724b546c182
incordios <69522766+incordios@users.noreply.github.com>
2022-04-09 15:44:39 -0400

    prereq JSON parsin

--- iOSClubScheduler/FBModel.swift
+++ iOSClubScheduler/FBModel.swift
@@ -7,6 +7,137 @@
 
 import Foundation
 
+
+/*
+ import UIKit
+ import Foundation
+
+ let json = """
+ [
+     {
+         "identifier": "COA 8843",
+         "prerequisites": {
+             "type": "and",
+             "courses": [
+                 {
+                     "type": "or",
+                     "courses": [
+                         "CHEM 1212K",
+                         "CHEM 1312",
+                         "CHEM 1313",
+                         "CHEM 2211"
+                     ]
+                 },
+                 "CHEM 2311",
+                 {
+                     "type": "or",
+                     "courses": [
+                         "CHEM 2312",
+                         "CHEM 2313"
+                     ]
+                 }
+             ]
+         }
+     }
+ ]
+ """.data(using: .utf8)!
+
+ // This code fails for more than one level of nesting
+ struct CourseInformation: Decodable {
+     let className: String
+     // add all fields
+     let prereq: Prerequisite
+
+     enum CodingKeys: String, CodingKey {
+         case className = "identifier"
+         case prereq = "prerequisites"
+     }
+ }
+
+ struct Prerequisite: Decodable {
+     let type: String
+     let courseList: MixedObjectCourseList
+     
+     enum CodingKeys: String, CodingKey {
+         case type = "type"
+         case courseList = "courses"
+     }
+ }
+
+ struct MixedObjectCourseList: Decodable {
+     var objects: [ObjectType] = []
+     
+     var flatten: [ObjectType] {
+         var flatPrereqs: [ObjectType] = []
+         for prereq in self.objects {
+             switch (prereq) {
+             case .innerItem(let innerItem):
+                 if innerItem.innerType == "and" {
+                     innerItem.innerCourseList.map {
+                         flatPrereqs.append(ObjectType.course($0))
+                     }
+                 } else if innerItem.innerType == "or" {
+                     flatPrereqs.append(ObjectType.innerItem(innerItem))
+                 }
+             case .course(_):
+                 flatPrereqs.append(prereq)
+             }
+         }
+         return flatPrereqs
+     }
+     
+     init(from decoder: Decoder) throws {
+         var container = try decoder.unkeyedContainer()
+         while !container.isAtEnd {
+             if let x = try? container.decodeIfPresent(String.self) {
+                 objects.append(.course(x))
+             } else if let x = try? container.decodeIfPresent(InnerItem.self) {
+                 objects.append(.innerItem(x))
+             } else {
+                 throw DecodingError.typeMismatch(Prerequisite.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "courses[] contains item that is not a course, like ECE 2020 or another list of courses, like {{type : or}, {courses : [ECE 2020, ECE 3020]}}"))
+             }
+         }
+     }
+ }
+         
+ struct InnerItem: Decodable {
+     let innerType: String
+     let innerCourseList: [String]
+             
+     enum CodingKeys: String, CodingKey {
+         case innerType = "type"
+         case innerCourseList = "courses"
+     }
+ }
+     
+ enum ObjectType {
+     case course(String)
+     case innerItem(InnerItem)
+ }
+
+ let decoder = JSONDecoder()
+ let payload = try decoder.decode([CourseInformation].self, from: json)
+ let prereqs = payload[0].prereq
+ var flatPrereqs: [ObjectType] = []
+ for prereq in prereqs.courseList.objects {
+     switch (prereq) {
+     case .innerItem(let innerItem):
+         flatPrereqs.append(prereqs.courseList.flatten( [ObjectType](innerItem.innerCourseList)))
+     case .course(let course):
+         flatPrereqs.append(prereq)
+     }
+ }
+ print(flatPrereqs)
+
+
+
+ 
+ 
+ 
+ */
+
+
+
 //"sections": [
 //{
 //"section_id": "A",
